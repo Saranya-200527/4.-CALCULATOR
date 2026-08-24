cbf31761747b37413a0bfaa2d80186480b4bd250
SARANYA S <25mca00py0006@pondiuni.ac.in>
2026-08-24 20:28:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,89 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+
+    if (display.value === "0") {
+        display.value = value;
+    }
+    else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    display.value = "0";
+}
+
+function deleteLast() {
+
+    if (display.value.length > 1) {
+        display.value = display.value.slice(0, -1);
+    }
+    else {
+        display.value = "0";
+    }
+}
+
 function calculate() {
 
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let operator = document.getElementById("operator").value;
+    let expression = display.value;
+
+    // Find the operator
+    let operator;
+
+    if (expression.includes("+")) {
+        operator = "+";
+    }
+    else if (expression.includes("-")) {
+        operator = "-";
+    }
+    else if (expression.includes("*")) {
+        operator = "*";
+    }
+    else if (expression.includes("/")) {
+        operator = "/";
+    }
+    else if (expression.includes("%")) {
+        operator = "%";
+    }
+
+    let numbers = expression.split(operator);
+
+    let num1 = Number(numbers[0]);
+    let num2 = Number(numbers[1]);
 
     let result;
 
-    if (operator === "+") {
-        result = num1 + num2;
-    }
-    else if (operator === "-") {
-        result = num1 - num2;
-    }
-    else if (operator === "*") {
-        result = num1 * num2;
-    }
-    else if (operator === "/") {
-        if (num2 === 0) {
-            result = "Cannot divide by zero";
-        } else {
-            result = num1 / num2;
-        }
+    switch (operator) {
+
+        case "+":
+            result = num1 + num2;
+            break;
+
+        case "-":
+            result = num1 - num2;
+            break;
+
+        case "*":
+            result = num1 * num2;
+            break;
+
+        case "/":
+            if (num2 === 0) {
+                result = "Cannot divide";
+            }
+            else {
+                result = num1 / num2;
+            }
+            break;
+
+        case "%":
+            result = num1 % num2;
+            break;
+
+        default:
+            result = "Invalid";
     }
 
-    document.getElementById("result").innerText = result;
+    display.value = result;
 }
